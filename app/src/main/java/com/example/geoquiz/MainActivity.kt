5d5a6a34b6d137ee7b6a8a06dd400c6d7bfa2208
Gary Dong <garydong@Garys-MacBook-Pro.local>
2024-02-02 22:42:02 -0500

    added in next buttons and checkAnswer

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -5,33 +5,77 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
-import android.widget.TextView
+import com.example.geoquiz.databinding.ActivityMainBinding
 
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var helloButton: Button
-    private lateinit var mainText: TextView
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        helloButton = findViewById(R.id.hello_button)
-        mainText = findViewById(R.id.main_text)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
 
 
-        helloButton.setOnClickListener { view: View ->
+
+        //Toasts
+        binding.trueButton.setOnClickListener { view: View ->
             // Do something in response to the click here
-            Toast.makeText(
-                this,
-                R.string.hello_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+           checkAnswer(true)
+        }
 
-            //Set text
-            mainText.setText("Hello! :)")
+        binding.falseButton.setOnClickListener { view: View ->
+            // Do something in response to the click here
+           checkAnswer(false)
 
         }
 
-    } }
+        //Makes the next button functional
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+
+    }
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    //function used to check answer
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(
+            this,
+            messageResId,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+}
+
